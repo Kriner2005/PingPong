54f8dd64b87ba540a9c4a42d40cba3e51525e40b
keiner <keiner.alberto@uptc.edu.co>
2026-05-04 21:28:24 -0500

    statsPanel platilla + componentes

--- src/main/java/co/edu/uptc/view/panels/StatsPanel.java
+++ src/main/java/co/edu/uptc/view/panels/StatsPanel.java
@@ -1,9 +1,13 @@
 package co.edu.uptc.view.panels;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -23,34 +27,53 @@ public class StatsPanel extends JPanel {
 
     private void initPanel() {
         setPreferredSize(new Dimension(250, 800));
+        setLayout(new BorderLayout());
         setBackground(Color.blue);
     }
 
     private void addComponents() {
+
+        Box labels = Box.createVerticalBox();
         addTimeStart();
         addTimerLabel();
-        addBtnReset();
-        addBtnPaused();
-        addBtnAddBall();
+        Box buttons = Box.createVerticalBox();
+        addBtnReset(buttons);
+        addBtnPaused(buttons);
+        addBtnAddBall(buttons);
     }
 
     private void addTimerLabel() {
-
+        Box box = Box.createVerticalBox();
+        JLabel text = new JLabel("Tiempo transcurrido:");
+        timer = new JLabel("00:00:00");
+        box.add(text);
+        box.add(timer);
+        add(box);
     }
 
     private void addTimeStart() {
+        Box box = Box.createVerticalBox();
+        JLabel text = new JLabel("Hora de inicio:");
+        startTimeGame = new JLabel("xx:xx:xx");
+        box.add(text);
+        box.add(startTimeGame);
+
+        add(box);
 
     }
 
-    private void addBtnReset() {
-
+    private void addBtnReset(Box box) {
+        JButton btnreset = new JButton("Reiniciar");
+        box.add(btnreset);
     }
 
-    private void addBtnPaused() {
-
+    private void addBtnPaused(Box box) {
+        JButton btnPaused = new JButton("Pausar");
+        box.add(btnPaused);
     }
 
-    private void addBtnAddBall() {
-
+    private void addBtnAddBall(Box box) {
+        JButton btnAddBall = new JButton("Agregar peltota");
+        box.add(btnAddBall);
     }
 }
